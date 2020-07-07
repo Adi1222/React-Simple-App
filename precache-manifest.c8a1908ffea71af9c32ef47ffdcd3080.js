86a54297e2ae8eee07facfa8da0f08683f5fc94b
Aditya Chavan <adityachavan677@gmail.com>
2020-07-07 18:31:24 +0530

    Updates

---
+++ precache-manifest.c8a1908ffea71af9c32ef47ffdcd3080.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "509d3e1c8a070e8ef4a1b6d5902e6e78",
+    "url": "/React-Simple-App/index.html"
+  },
+  {
+    "revision": "dcf50660bc298dd13b4b",
+    "url": "/React-Simple-App/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9d2ae3c2628a4c5fd0d4",
+    "url": "/React-Simple-App/static/js/2.67691ccd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Simple-App/static/js/2.67691ccd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcf50660bc298dd13b4b",
+    "url": "/React-Simple-App/static/js/main.f183ad6b.chunk.js"
+  },
+  {
+    "revision": "7e936e361da16770847c",
+    "url": "/React-Simple-App/static/js/runtime-main.96e65aa7.js"
+  }
+]);
